05e3af6b4e57f49752178babcfb5c3cc3290a36e
Dhanesh Rao <dhaneshrao@Dhaneshs-Laptop.lan>
2025-07-15 11:54:06 -0400

    changes1

--- src/main/java/com/example/employees/EmployeesApplication.java
+++ src/main/java/com/example/employees/EmployeesApplication.java
@@ -7,8 +7,6 @@ import com.example.employees.service.MyFunction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.nio.file.LinkOption;
 import java.util.ArrayList;
@@ -18,15 +16,13 @@ import java.util.function.*;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
-public class EmployeesApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(EmployeesApplication.class);
-	}
+public class EmployeesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeesApplication.class, args);
-	}
-}
 
+		SpringApplication.run(EmployeesApplication.class, args);
+
+
+	}
+
+}
